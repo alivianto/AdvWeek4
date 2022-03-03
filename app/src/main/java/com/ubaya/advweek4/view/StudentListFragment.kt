be168f8a2ad04501d9e1f7c692c0606b73f28d38
Andi L <alivianto99@gmail.com>
2022-03-03 14:13:08 +0700

    view

---
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -0,0 +1,23 @@
+package com.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ubaya.advweek4.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StudentListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StudentListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+}
